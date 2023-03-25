7be4bfe1e0b505506f32ff0d1203f60a85005c26
Oliver Andrews <oltandrews@gmail.com>
2023-03-25 10:44:48 -0700

    test fix jet list index

--- components/JetList.tsx
+++ components/JetList.tsx
@@ -15,9 +15,9 @@ const JetList: React.FC<JetListProps> = ({ jets }) => {
       className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
     >
       {jets.map((jet, index) => (
-        <div key={index}>
-          <JetCard jet={jet} />
-        </div>
+
+          <JetCard key={index} jet={jet} />
+
       ))}
     </div>
   );
